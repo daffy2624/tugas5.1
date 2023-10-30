8fa63a9a6656f67daeeaed8829206fae147798c9
daffy26241 <daffy2624@gmail.com>
2023-10-30 20:08:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas51/ItemDao.java
@@ -0,0 +1,27 @@
+package com.example.tugas51;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ItemDao {
+    @Query("SELECT * FROM item")
+    List<Item> getAll();
+
+    @Insert
+    void insertAll(Item... items);
+
+    @Delete
+    void delete(Item item);
+
+    @Update
+    void update(Item item);
+
+}
+
+
